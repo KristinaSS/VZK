084aa1ef95fab565fce52c9714df5e67f6f0a71e
Kristina Stoyanova <krisagriza@gmail.com>
2023-08-27 22:57:38 +0300

    added news microservice

---
+++ news/src/main/resources/changelog/changes/1_create_tables.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- Changeset kristina:2023-08-13-create-events-table
+CREATE TABLE `vzk_news`.`article` (
+                                      `id` INT NOT NULL AUTO_INCREMENT,
+                                      `date` VARCHAR(45) NOT NULL,
+                                      `description` VARCHAR(1000) NOT NULL,
+                                      `image` VARCHAR(100) NULL,
+                                      PRIMARY KEY (`id`));
+
