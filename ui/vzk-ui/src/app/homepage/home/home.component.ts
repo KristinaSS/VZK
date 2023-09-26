02c009d728df1f577f12eb992faf924818c20c35
Kristina Stoyanova <krisagriza@gmail.com>
2023-09-27 00:33:27 +0300

    made viewbox dynamic

--- ui/vzk-ui/src/app/homepage/home/home.component.ts
+++ ui/vzk-ui/src/app/homepage/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  screenWidth: number | undefined;
+  screenHeight: number | undefined;
 
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit() {
+    // Get the current screen width and height
+    this.screenWidth = this.document.documentElement.clientWidth;
+    this.screenHeight = this.document.documentElement.clientHeight;
+  }
 }
