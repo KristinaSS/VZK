fe527a80fa9ddd8f2ea3e837c8483cb27028df30
Kristina Stoyanova <krisagriza@gmail.com>
2023-10-02 23:40:33 +0300

    fixing smaller window mode: navbar and welcome box

--- ui/vzk-ui/src/app/general/navbar/navbar.component.ts
+++ ui/vzk-ui/src/app/general/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NavbarComponent {
   isMobileMenuActive = false;
