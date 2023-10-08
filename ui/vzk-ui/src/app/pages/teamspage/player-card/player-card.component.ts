871cd7ed6a4a9dfc1e067d5c75f6ea1db3ac293f
Kristina Stoyanova <krisagriza@gmail.com>
2023-10-09 00:50:58 +0300

    need to center player name and no pink

--- ui/vzk-ui/src/app/pages/teamspage/player-card/player-card.component.ts
+++ ui/vzk-ui/src/app/pages/teamspage/player-card/player-card.component.ts
@@ -1,32 +1,11 @@
-// player-card.component.ts
-
-import { Component, Input, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Player} from "../../../models/player/player";
 
 @Component({
   selector: 'app-player-card',
   templateUrl: './player-card.component.html',
   styleUrls: ['./player-card.component.css']
 })
-export class PlayerCardComponent implements AfterViewInit {
-  @Input() player: any; // Assuming 'player' is an Input property
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-
-  ngAfterViewInit() {
-    this.rotateText();
-  }
-
-  private rotateText() {
-    const textElement = this.el.nativeElement.querySelector('.text p');
-
-    if (textElement) {
-      const originalText: string = textElement.innerText; // Add type annotation here
-      const rotatedText = originalText
-        .split('')
-        .map((char: string, i: number) => `<span style="transform:rotate(${i * 8}deg)">${char}</span>`)
-        .join('');
-
-      this.renderer.setProperty(textElement, 'innerHTML', rotatedText);
-    }
-  }
+export class PlayerCardComponent {
+  @Input() player: Player | undefined;
 }
