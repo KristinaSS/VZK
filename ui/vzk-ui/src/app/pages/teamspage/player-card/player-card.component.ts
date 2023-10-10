ce89d5a3ad5138f1625792b56343e2764aa91fd7
Kristina Stoyanova <krisagriza@gmail.com>
2023-10-10 20:12:22 +0300

    social media navigation works

--- ui/vzk-ui/src/app/pages/teamspage/player-card/player-card.component.ts
+++ ui/vzk-ui/src/app/pages/teamspage/player-card/player-card.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {Player} from "../../../models/player/player";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-player-card',
@@ -8,4 +9,30 @@ import {Player} from "../../../models/player/player";
 })
 export class PlayerCardComponent {
   @Input() player: Player | undefined;
+
+  constructor(private router: Router) {
+  }
+
+  redirectTo(platform: string): void {
+    switch (platform) {
+      case 'twitch':
+        this.navigateToExternalLink(this.player?.twitch);
+        break;
+      case 'twitter':
+        this.navigateToExternalLink(this.player?.twitter);
+        break;
+      case 'youtube':
+        this.navigateToExternalLink(this.player?.youtube);
+        break;
+      case 'instagram':
+        this.navigateToExternalLink(this.player?.instagram);
+        break;
+      default:
+        break;
+    }
+  }
+
+  private navigateToExternalLink(url: string | undefined): void {
+    window.open(url, '_blank');
+  }
 }
