13d8ef93e4d61ead049ac4f15bf7c84b8becdf7e
Kristina Stoyanova <krisagriza@gmail.com>
2023-10-25 15:01:33 +0300

    Form done

--- ui/vzk-ui/src/app/pages/contactpage/contact-form/contact-form.component.ts
+++ ui/vzk-ui/src/app/pages/contactpage/contact-form/contact-form.component.ts
@@ -3,6 +3,11 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Game} from '../../../models/game/game';
 import {GameService} from '../../../services/game-service/game.service';
 import {CountryService} from '../../../services/country-service/country.service';
+import {
+  ageValidator,
+  firstLetterUppercaseValidator,
+  urlValidator
+} from "../../../utils/validators/custom-contact-form-validators";
 
 @Component({
   selector: 'app-contact-form',
@@ -18,33 +23,42 @@ export class ContactFormComponent implements OnInit {
 
   contactForm!: FormGroup;
 
+  characterCount = 0;
+  maxCharacterLimit = 512;
+  isSubmitDisabled = true; // Initially, the button is disabled
+
   constructor(
     private fb: FormBuilder,
     private gameService: GameService,
     private countryService: CountryService,
     private cd: ChangeDetectorRef,
-    ) {
+  ) {
   }
 
   ngOnInit(): void {
     this.games = this.gameService.getGames();
 
     this.contactForm = this.fb.group({
-      fName: ['', Validators.required],
-      lName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      fName: ['', [Validators.required, Validators.minLength(3), firstLetterUppercaseValidator(), Validators.maxLength(50)]],
+      lName: ['', [Validators.required, Validators.minLength(3), firstLetterUppercaseValidator(), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(100)]],
       topic: ['', Validators.required],
       comments: ['', Validators.required],
       applicationType: ['', Validators.required],
-      profileURL: ['', Validators.required],
+      profileURL: ['', [Validators.required, urlValidator()]],
       game: ['', Validators.required],
       role: ['', Validators.required],
       maxRank: ['', Validators.required],
       gender: ['', Validators.required],
-      birthday: ['', Validators.required],
+      birthday: ['', [Validators.required, ageValidator(16)]],
       country: ['', Validators.required],
       description: ['', Validators.required],
-      consent: ['', Validators.required],
+      consent: false,
+    });
+
+    // Subscribe to form value changes and update the button state
+    this.contactForm.valueChanges.subscribe(() => {
+      this.updateSubmitButtonState();
     });
   }
 
@@ -56,19 +70,114 @@ export class ContactFormComponent implements OnInit {
     return this.gameService.getGameRanks(gameId);
   }
 
-  submitForm() {
-    // Add logic to handle form submission (e.g., send data to a server)
-    console.log('Form submitted:', this.contactForm.value);
-  }
-
   onGameChange() {
-    console.log('Game changed:', this.contactForm.value.game);
     this.roles = this.getRoles(this.contactForm.value.game);
     this.ranks = this.getRanks(this.contactForm.value.game);
 
     this.countryService.getCountries().subscribe((data) => {
       this.countries = data.sort((a, b) => a.name.localeCompare(b.name));
     });
+
+    const fieldsToClear = ['profileURL', 'role', 'maxRank'];
+
+    this.clearFields(fieldsToClear);
+
     this.cd.detectChanges();
   }
+
+  onTopicChange() {
+    const fieldsToClear = ['game', 'applicationType', 'profileURL', 'role', 'maxRank', 'gender', 'birthday', 'country', 'description', 'comments'];
+
+    this.clearFields(fieldsToClear);
+  }
+
+  onApplicationTypeChange() {
+    const fieldsToClear = ['profileURL', 'role', 'maxRank'];
+
+    this.clearFields(fieldsToClear);
+  }
+
+  onInput(event: Event, textArea: string): void {
+    const textareaValue = (event.target as HTMLTextAreaElement).value;
+    this.characterCount = textareaValue.length;
+
+    // Disable textarea when character limit is reached
+    if (this.characterCount > this.maxCharacterLimit) {
+      const truncatedValue = textareaValue.slice(0, this.maxCharacterLimit);
+      this.contactForm.get(textArea)?.setValue(truncatedValue);
+      this.characterCount = this.maxCharacterLimit;
+    }
+  }
+
+
+  updateSubmitButtonState() {
+    if (this.contactForm) {
+      this.isSubmitDisabled = !((
+          this.contactForm.get('consent')?.value == true &&
+          this.contactForm.get('fName')?.valid &&
+          this.contactForm.get('lName')?.valid &&
+          this.contactForm.get('email')?.valid &&
+          this.contactForm.get('topic')?.valid &&
+          this.contactForm.get('comments')?.valid)
+        ||
+        (
+          this.contactForm.get('fName')?.valid &&
+          this.contactForm.get('lName')?.valid &&
+          this.contactForm.get('email')?.valid &&
+          this.contactForm.get('topic')?.valid &&
+          this.contactForm.get('game')?.valid &&
+          this.contactForm.get('applicationType')?.valid &&
+          this.contactForm.get('gender')?.valid &&
+          this.contactForm.get('birthday')?.valid &&
+          this.contactForm.get('country')?.valid &&
+          this.contactForm.get('description')?.valid &&
+          this.contactForm.get('consent')?.value == true
+        )
+        ||
+        (
+          this.contactForm.get('fName')?.valid &&
+          this.contactForm.get('lName')?.valid &&
+          this.contactForm.get('email')?.valid &&
+          this.contactForm.get('topic')?.valid &&
+          this.contactForm.get('game')?.valid &&
+          this.contactForm.get('applicationType')?.valid &&
+          this.contactForm.get('profileURL')?.valid &&
+          this.contactForm.get('role')?.valid &&
+          this.contactForm.get('maxRank')?.valid &&
+          this.contactForm.get('gender')?.valid &&
+          this.contactForm.get('birthday')?.valid &&
+          this.contactForm.get('country')?.valid &&
+          this.contactForm.get('description')?.valid &&
+          this.contactForm.get('consent')?.value == true
+        ));
+    }
+  }
+
+
+
+  submitForm() {
+    // Add logic to handle form submission (e.g., send data to a server)
+    console.log('Form submitted:', this.contactForm.value);
+  }
+
+  private clearFields(fieldsToClear: string[]) {
+    fieldsToClear.forEach(controlName => {
+      const control = this.contactForm.get(controlName);
+
+      // Check if control is not null
+      if (control) {
+        // Reset value
+        control.setValue('');
+
+        // Mark as untouched and pristine
+        control.markAsUntouched();
+        control.markAsPristine();
+
+        // Update value and validity
+        control.updateValueAndValidity();
+      }
+    });
+  }
+
+
 }
---
+++ ui/vzk-ui/src/app/utils/validators/custom-contact-form-validators.ts
@@ -0,0 +1,56 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function firstLetterUppercaseValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value: string = control.value as string;
+
+    if (value && value.length >= 3) {
+      const firstLetter = value.charAt(0);
+      if (firstLetter !== firstLetter.toUpperCase()) {
+        return { firstLetterUppercase: true };
+      }
+    }
+    return null;
+  };
+}
+
+// Custom URL validator function
+export function urlValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (!control.value) {
+      // If the control value is empty, consider it valid
+      return null;
+    }
+
+    // Regular expression for a simple URL validation
+    const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+
+    // Check if the control value matches the URL pattern
+    const isValid = urlPattern.test(control.value);
+
+    // If the value is valid, return null; otherwise, return a validation error
+    return isValid ? null : {invalidUrl: true};
+  };
+}
+
+export function ageValidator(minAge: number): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (!control.value) {
+      // If the control has no value, consider it valid (you can adjust this according to your needs)
+      return null;
+    }
+
+    const birthDate = new Date(control.value);
+    const currentDate = new Date();
+
+    const age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    if (currentDate.getMonth() < birthDate.getMonth() ||
+      (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() < birthDate.getDate())) {
+      // Subtract 1 from age if the birthdate hasn't occurred yet this year
+      return age - 1 < minAge ? { 'minAge': true } : null;
+    }
+
+    return age < minAge ? { 'minAge': true } : null;
+  };
+}
