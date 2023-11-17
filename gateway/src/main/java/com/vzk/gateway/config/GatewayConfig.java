46b0bee9dec6e50e84c2c9354365d4885b294006
Kristina Stoyanova <krisagriza@gmail.com>
2023-11-17 19:02:27 +0200

    help

---
+++ gateway/src/main/java/com/vzk/gateway/config/GatewayConfig.java
@@ -0,0 +1,30 @@
+package com.vzk.gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableHystrix
+@RequiredArgsConstructor
+public class GatewayConfig {
+
+    private final AuthenticationFilter filter;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("cars-service", r -> r.path("/cars/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://cars-service"))
+
+                .route("auth-service", r -> r.path("/auth/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://auth-service"))
+                .build();
+    }
+
+}
