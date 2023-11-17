43f9f0a5cc3a8d808b2a919d56a106ab0be13cd6
Kristina Stoyanova <krisagriza@gmail.com>
2023-11-17 17:23:39 +0200

    Added and created gateway api. Works

--- security/src/main/java/com/vzk/security/utils/Constants.java
+++ security/src/main/java/com/vzk/security/utils/Constants.java
@@ -3,7 +3,7 @@ package com.vzk.security.utils;
 import java.util.UUID;
 
 public class Constants {
-    public static final String ACCOUNT_SERVICE_BASE_URL = "http://localhost:8080";
+    public static final String ACCOUNT_SERVICE_BASE_URL = "http://localhost:8082";
 
     public static final String ACCOUNT_GET_ACCOUNT_BY_EMAIL_URL = "/account/email/{email}";
 
@@ -12,7 +12,7 @@ public class Constants {
     public static final String ACCOUNT_SERVICE_NAME = "account-service";
 
 
-    public static final String ROLES_SERVICE_BASE_URL = "http://localhost:8084";
+    public static final String ROLES_SERVICE_BASE_URL = "http://localhost:8083";
 
     public static final String ROLES_GET_ROLES_BY_ACC_ID_URL = "/role/account/{id}";
 
